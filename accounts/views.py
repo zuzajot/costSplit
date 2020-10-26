c557b7663356eb6b9d005d73c47d71e5487f5a9b
lukasz-trener <lukasz.okon@outlook.com>
2020-10-26 19:11:03 +0100

    *Dodano folder template *Dodano folder template/accounts *Dodano plik template/accounts/login.html *Poprawiono formatowanie pliku accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -24,6 +24,7 @@ class PasswordReset(PasswordResetView):
     email_template_name = 'accounts/password_reset_form.html'
     success_url = reverse_lazy('accounts/password_reset/')
 
+
 class PasswordResetDone(PasswordResetDoneView):
     template_name = 'accounts/password_reset_done.html'
     success_url = reverse_lazy('accounts/password_reset/done/')
@@ -32,13 +33,16 @@ class PasswordResetConfirm(PasswordResetConfirmView):
     template_name = 'accounts/password_reset_confirm.html'
     success_url = reverse_lazy('reset/<uidb64>/<token>')
 
+
 class PasswordResetComplete(PasswordResetCompleteView):
     template_name = 'accounts/password_reset_complete.html'
     success_url = reverse_lazy('reset/done/')
 
+
 class PasswordChange(PasswordChangeView):
     template_name = 'accounts/password_change.html'
     success_url = reverse_lazy('accounts/password_change/')
+
 
 class PasswordChangeDone(PasswordChangeDoneView):
     template_name = 'accounts/password_change_done.html'
