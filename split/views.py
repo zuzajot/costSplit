8146865388ebd673f1c00d9e2269e7e253008f35
Łukasz <lukaszweglarz385@gmail.com>
2020-10-28 18:07:46 +0100

    Widok tworzenia grup tworzy teraz również obiekt modelu GroupUser.

--- split/views.py
+++ split/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView
 from .models import GroupUser, Group
+from . import db_operations as db
 
 
 # Create your views here.
@@ -24,4 +26,6 @@ class CreateGroupView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.admin_id = self.request.user.profile
+        form.instance.save()
+        GroupUser(user_id=self.request.user.profile, group_id=form.instance).save()
         return super().form_valid(form)
