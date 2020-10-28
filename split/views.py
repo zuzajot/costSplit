b024693ffd08ec67ee5308ea4865f52a03985ff7
Łukasz <lukaszweglarz385@gmail.com>
2020-10-28 18:54:15 +0100

    Dodałem generowanie linków do zaproszeń.

--- split/views.py
+++ split/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView
 from .models import GroupUser, Group
-from . import db_operations as db
-
+import string
+import random
+from django.urls import reverse
 
 # Create your views here.
 
@@ -24,8 +24,17 @@ class CreateGroupView(LoginRequiredMixin, CreateView):
     fields = ["name"]
     success_url = "/groups"
 
+    def generate_unique_url(self, length):
+        urls = self.model.objects.all().values_list('invite_url', flat=True)
+        while True:
+            invite_url = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+            if invite_url in urls:
+                continue
+            return invite_url
+
     def form_valid(self, form):
         form.instance.admin_id = self.request.user.profile
+        form.instance.invite_url = self.generate_unique_url(10)
         form.instance.save()
         GroupUser(user_id=self.request.user.profile, group_id=form.instance).save()
         return super().form_valid(form)
